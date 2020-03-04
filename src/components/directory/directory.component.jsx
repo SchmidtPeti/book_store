69ea5b98d3f2d2550d617dbab69ab23bb36a2db5
Whichhunter404 <schmidt.peti32@gmail.com>
2020-03-04 16:34:52 +0100

    making directories

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+class Directory extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            sections : [
+                {
+                    title: 'Classics books',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1,
+                    linkUrl: 'shop/hats'
+                },
+                {
+                    title: 'Comic books',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2,
+                    linkUrl: 'shop/jackets'
+                },
+                {
+                    title: 'Fairy tails',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3,
+                    linkUrl: 'shop/sneakers'
+                },
+                {
+                    title: 'Meaningful books',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: 'shop/womens'
+                },
+                {
+                    title: 'Light books',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className={'directory-menu'}>
+                {
+                    this.state.sections.map(({id,title,imageUrl}) =>
+                    <MenuItem key={id} title={title} imageUrl={imageUrl}/>
+                    )
+                }
+            </div>
+        );
+    }
+}
+export default Directory;
