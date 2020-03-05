15a5b08badf8227fb5db2b961fa268d4a2886a49
Whichhunter404 <schmidt.peti32@gmail.com>
2020-03-05 19:36:05 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import {Route,Switch} from 'react-router-dom';
+
+const Hats = () =>(
+    <div>
+        <h1>Hats page</h1>
+    </div>
+);
 
 function App() {
   return (
-    <div className="container">
-      <Homepage></Homepage>
+    <div>
+        <Switch>
+            <Route exact={true} path={'/'} component={Homepage}/>
+            <Route exact={true} path={'/hats'} component={Hats}/>
+        </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './menu-item.style.scss';
 
-const MenuItem = ({title,imageUrl}) =>{
+const MenuItem = ({title,imageUrl,size}) =>{
     return(
-        <div style={{backgroundImage: `url(${imageUrl})`}} className={'menu-item'}>
+        <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
         <div className={'content'}>
-            <h1 className={'title'}>{title}</h1>
+            <h1 className={'title'}>{title.toUpperCase()}</h1>
             <span className={'subtitle'}>Shop now</span>
         </div>
     </div>);
