c15aa4f8fbaefd30412c5fad104f4fe1532a326c
Whichhunter404 <schmidt.peti32@gmail.com>
2020-03-08 12:03:20 +0100

    adding some new components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import {Route,Switch} from 'react-router-dom';
+import Header from './components/header/header.component';
+import SignInAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 
 function App() {
   return (
     <div>
+        <Header/>
         <Switch>
             <Route exact={true} path={'/'} component={Homepage}/>
             <Route exact={true} path={'/shop'} component={ShopPage}/>
+            <Route exact={true} path={'/signin'} component={SignInAndSignOut}/>
         </Switch>
     </div>
   );
